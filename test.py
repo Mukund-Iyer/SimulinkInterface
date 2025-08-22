405c52af702adb716855b6accf4558d49740074d
Mukund-Iyer <mukundiyer1995@gmail.com>
2025-08-22 13:25:39 +0530

    Fixed test.py as it addressed a temporary file in the imports section.

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-import SimulinkParser_v2
-parser = SimulinkParser_v2.SimulinkModel("C:\Work\ModelDiffTool\L3DrvMon_MDCE_SHELL.slx")
-print(parser.block_list)
+import SimulinkInterface
+model = SimulinkInterface.SimulinkModel("C:\Work\ModelDiffTool\L3DrvMon_MDCE_SHELL.slx")
+print(model.block_list)
